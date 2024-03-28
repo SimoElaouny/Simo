782bb593054719e78ef48a97deff95513015f8d1
SimoElaouny <159307588+SimoElaouny@users.noreply.github.com>
2024-03-28 00:38:01 +0000

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -33,7 +33,7 @@ const defaultMenu = {
   header: '┏━━⊜ *_%category_* ',
   body: '┃⋄ %cmd %isdiamond %isPremium',
   footer: '┗━━━━━━━━⬣\n',
-  after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول  *\n+212717457920',
+  after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول  *\nhttps://instagram.com/simo__awni',
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
