b91bd1c26201075fce84f5a1daf5bbe55e4868f1
SimoElaouny <159307588+SimoElaouny@users.noreply.github.com>
2024-04-05 01:33:57 +0000

    Update main.js

--- main.js
+++ main.js
@@ -490,7 +490,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `S I M O E L | A O U N Y ®: ${uptime}`;
+  const bio = `J E F F R Y | H A M Z A ®: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
